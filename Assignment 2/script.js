3ed0c3313c339897a3a45116fef7c1f7edfb66b3
Momina Farooq <107984079+Momina-Farooq-045247@users.noreply.github.com>
2022-08-22 00:58:17 -0700

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,31 +1,53 @@
-document.querySelector('button').addEventListener('click',(e) => {
-    getCompletedTodos();
+// document.querySelector('#btn1').addEventListener('click', (e) => {
+//     getCompleted()
+// })
+// function getCompleted(){
+//     let values = []
+
+//     checked = document.querySelectorAll('input[type=checkbox]:checked')
+//     checked.forEach(chk => values.push(chk.value))
+//     console.log(values)
+// }
+let btnName = document.getElementById('btn1');
+btnName.addEventListener('click', ()=>{
+  let element = document.getElementById('elementId');
+	if (element.style.display != 'inline'){
+    	element.style.display = 'inline';
+    }
+  else{
+    element.style.display = 'none'
+  }
+})
+document.getElementById('btn4').style.visibility = 'visible';
+
+
+document.getElementById('btn1').style.visibility = 'visible';
+
+window.addEventListener('DOMContentLoaded',event=>{
+    div_data = document.querySelector('#contaniner')
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(element => {
+        div_data.innerHTML +=
+        `<input type="checkbox" id="${element.id}" ${element.completed?"checked":""}>
+        <label for="${element.id}"> ${element.title} </label><br/>`
+    });
+    getCompleted()
+  })
+  .catch(error => console.error(error));
 })
 
-function getCompletedTodos()
-{
-    let values = []
-    checked=document.querySelector('input[type="checkbox"]:checked')
-    checked.forEach(chk => values.push(chk.values))
-    console.log('values')
-        
-    
-}
+document.querySelector('#btn2').addEventListener('click', (e) => {
+    let checkAll = document.querySelectorAll('input[type=checkbox]')
+    checkAll.forEach(function(checkbox) {
+        checkbox.checked=true;
+    })
+})
 
-window.addEventListener('DOMContentLoaded',event =>
-{
-    div_todos = document.querySelector('#todos')
-
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(Response => Response.json())
-    .then(todos => 
-        {
-            todos.forEach(todos =>
-                {
-                    div_todos.innerHTML +=
-                    `<input type = "checkbox" value= "${todo.id}" id="${todo.id}" ${todo.completed?"checked":"" }/>
-                    <label for ="${todo.id}"> ${todo.title}</label> <br/>`
-                });
-        })
-    .catch(err => console.error(err));
+document.querySelector('#btn3').addEventListener('click', (e) => {
+    let uncheckall = document.querySelectorAll('input[type=checkbox]')
+    uncheckall.forEach(function(checkbox) {
+        checkbox.checked=false;
+    })
 })
